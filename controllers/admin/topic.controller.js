3b33e21c7664a5f7d7228a41c93335bd5bd9614f
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-11-15 17:31:35 +0700

    2. Làm giao diện trang quản lí chủ đề

--- controllers/admin/topic.controller.js
+++ controllers/admin/topic.controller.js
@@ -2,8 +2,15 @@ const Topic = require("../../models/topic.model");
 
 
 module.exports.index = async (req, res) => {
+  const find = {
+    deleted: false
+  }
   
+  const topics = await Topic.find(find);
+  
+
   res.render("admin/pages/topics/index", {
-    pageTitle: "Trang chu de"
+    pageTitle: "Trang chu de",
+    topics: topics
   })
 }
