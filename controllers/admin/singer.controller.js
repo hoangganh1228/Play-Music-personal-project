9042b1f2b2d769324c91592f2d3870252b6ee47c
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2024-11-19 20:54:43 +0700

    2. Làm chức năng thêm mới ca sĩ

--- controllers/admin/singer.controller.js
+++ controllers/admin/singer.controller.js
@@ -14,3 +14,29 @@ module.exports.index = async (req, res) => {
   })
 
 }
+
+// [GET]
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/singers/create", {
+    pageTitle: "Trang tạo mới ca sĩ",
+  })
+}
+
+// [POST]
+module.exports.createPost = async (req, res) => {
+  try {
+    const objectSinger = new Singer({
+      fullName: req.body.fullName,
+      status: req.body.status,
+      avatar: req.body.avatar,
+    })  
+  
+    objectSinger.save();
+    req.flash("success", `Thêm ca sĩ thành công!`)
+  } catch (error) {
+    req.flash("error", `Thêm ca sĩ thất bại!`)
+  }
+
+  res.redirect("back");
+  
+}
--- routes/admin/singer.route.js
+++ routes/admin/singer.route.js
@@ -8,4 +8,13 @@ const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
 
 router.get("/", controller.index);
 
+router.get("/create", controller.create);
+
+router.post(
+  "/create", 
+  upload.single('avatar'),
+  uploadCloud.uploadSingle,
+  controller.createPost)
+
+
 module.exports = router
